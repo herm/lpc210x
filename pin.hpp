5b942ef35870aefcf21a86c5d05444e881578643
Hermann Kraus <hermr2d2@gmail.com>
2013-04-19 11:43:12 +0200

    Add pin handling functions.

--- pin.hpp
+++ pin.hpp
@@ -1,14 +1,23 @@
 #ifndef PIN_HPP
 #define PIN_HPP
 
-class Pin
+#include <inttypes.h>
+#include <system/inline.hpp>
+
+#define pin_mask(pin)  (1<<(pin))
+#define pin_func(pin, function) ((function) << (((pin) & 0x0f)*2))
+#define pin_func_mask(pin) (3 << (((pin) & 0x0f)*2))
+
+class DigitalOut
 {
 public:
-    Pin(uint32_t bitmask) : bitmask_(bitmask) {}
-    void low() { FIOCLR = bitmask_;}
-    void high() { FIOSET = bitmask_;}
+    explicit DigitalOut(uint32_t pinnr) : bitmask_(1 << pinnr) { FIODIR |= bitmask_; }
+    void low() const { FIOCLR = bitmask_;}
+    void high() const { FIOSET = bitmask_;}
+    force_inline void operator=(int value) const { if (value) FIOSET = bitmask_; else FIOCLR = bitmask_; }
+    force_inline operator int() const { return FIOPIN & bitmask_ ? 1 : 0; }
 private:
-    uint32_t bitmask_;
+    const uint32_t bitmask_;
 };
 
 

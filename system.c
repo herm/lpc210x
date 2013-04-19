8235c4ba2d3ce7e08b2e81ed5d47ed24efed58aa
Hermann Kraus <hermr2d2@gmail.com>
2013-04-19 11:35:23 +0200

    Improved system init.

--- system.c
+++ system.c
@@ -1,11 +1,27 @@
 #include "config.h"
-#include "lpc2103.h"
+#include "../lpc2103.h"
 #include "system.h"
 
 void system_init(void)
 {
+#if APBDIVIDER == 1
     APBDIV = APBDIV1;
-#ifdef PLL
+#else
+#error Unkown APB divider
+#endif
+
+#if MCLK > 40000000
+    MAMTIM = 3;
+#elif MCLK > 20000000
+    MAMTIM = 2;
+#else
+    MAMTIM = 1;
+#endif
+#if MAM_MODE != 0
+    MAMCR = MAM_MODE;
+#endif
+
+#if PLL_M != 1
     PLLCFG = MSEL | PSEL; //defined in config.h
     PLLCON = PLLE;
     PLLFEED = PLL_FEED1;
@@ -15,10 +31,6 @@ void system_init(void)
     PLLFEED = PLL_FEED1;
     PLLFEED = PLL_FEED2;
 #endif
-#ifdef MAM
-    MAMTIM = 3;
-    MAMCR = 2;
-#endif
 
 #if defined(RAM_RUN)
     MEMMAP = MEMMAP_USER_RAM_MODE;
@@ -27,11 +39,7 @@ void system_init(void)
 #else
 #error RUN_MODE not defined!
 #endif
-}
-
-void gpio_init(void)
-{
-    SCS = GPIO0M;
+    SCS = GPIO0M; //Fast GPIO
 #if default_PINSEL0 != 0
     PINSEL0 = default_PINSEL0;
 #endif

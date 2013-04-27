7906271eee5e6a2e808c0301504aebc2f407c15e
Hermann Kraus <hermr2d2@gmail.com>
2013-04-27 22:42:27 +0200

    Add nop() command.

--- delay.h
+++ delay.h
@@ -3,9 +3,10 @@
 #include "config.h"
 #define TUNE_FAC 0.96154
 
-#include <inttypes.h>
+#include <stdint.h>
 
-#define delay_us(delayval) delay_us_( (uint32_t)( (FOSC*PLL_M) / (1000000/delayval) /4 * TUNE_FAC) ) 
+#define delay_ms(delayval) delay_us(delayval*1000)
+#define delay_us(delayval) delay_us_( (uint32_t)((MCLK)/ (1000000/(delayval)) /4 * TUNE_FAC) )
 void delay_us_(uint32_t delayval);
-
+#define nop() asm volatile("nop")
 #endif
